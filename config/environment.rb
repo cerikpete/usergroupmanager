791f72a683630d90cfc13d7b7bb8e9b460f21047
Erik Peterson <cerikpete@gmail.com>
2010-04-19 21:21:04 -0400

    Updated environment settings to include mongo gems and remove the active record gem Fixed production db name in the database config

--- config/environment.rb
+++ config/environment.rb
@@ -26,10 +26,12 @@ Rails::Initializer.run do |config|
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  config.gem 'mongo'
+  config.gem 'mongo_mapper'
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
