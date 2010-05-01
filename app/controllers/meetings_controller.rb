dbfba144f592da6f663ba065e70aae17f8d71a75
Erik Peterson <cerikpete@gmail.com>
2010-04-30 21:18:02 -0400

    Implemented creating a new task, and linked to it on the index page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -2,6 +2,15 @@ class MeetingsController < ApplicationController
   def index
     @meetings = Meeting.all
   end
+  
+  def create
+    @meeting = Meeting.new(params[:meeting])
+    if @meeting.save
+      redirect_to meetings_path
+    else
+      render :new, :object => @meeting
+    end
+  end
 
   def edit
     @meeting = Meeting.find(params[:id])    
