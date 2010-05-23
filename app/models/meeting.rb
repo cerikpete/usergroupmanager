fa45d5fc5400ec21f0bc943d9096c1d110701b2e
Erik Peterson <cerikpete@gmail.com>
2010-05-23 18:04:33 -0400

    Updated the meeting_date field to be a date instead of a string

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,7 +2,7 @@ class Meeting
   include MongoMapper::Document
 
   key :title, String
-  key :meeting_date, String
+  key :meeting_date, Date
   key :location, String
   key :presenter, String
   key :description, String
