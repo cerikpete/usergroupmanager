1217b1a6b2f6c27ac20a7fa79bce9d55d9adb76d
Erik Peterson <cerikpete@gmail.com>
2010-05-23 18:34:55 -0400

    Removed location field from the meetings domain object and added host company and address

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -3,7 +3,8 @@ class Meeting
 
   key :title, String
   key :meeting_date, Date
-  key :location, String
+  key :host_company, String
+  key :address, String
   key :presenter, String
   key :description, String
   timestamps!
